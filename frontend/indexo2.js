a111af0fdc3826ba72a609d959315fdddd3176a5
Aris <arislebechec@gmail.com>
2021-07-26 18:33:32 +0200

    index.js et html

---
+++ frontend/indexo2.js
@@ -0,0 +1,29 @@
+// recupération des produit de L'API. D
+fetch('http://localhost:3000/api/cameras')
+.then(function(reponse){
+    return reponse.json();
+})
+//Alternatif d'appelle de fonction : .then((reponse) => { return reponse.json()});
+.then (function(data){
+    console.log(data);
+
+for (let i = 0; i < data.length; i++) {
+    let name = data[i].name;
+    let contener = document.querySelector(".test")
+    let div = document.createElement("div");
+    div.innerHTML = `<p>Je suis la div créer ${name} </p>`;
+    contener.appendChild(div);
+}
+
+})
+//Alternatif d'apelle de fonction : .then((data) =>{console.log(data);})
+
+// recupération des produit de L'API. F
+
+
+
+
+
+
+
+
