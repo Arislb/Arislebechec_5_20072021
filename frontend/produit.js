251df3661ea240f44c51409c64145693429dbad2
Aris <arislebechec@gmail.com>
2021-07-28 17:50:13 +0200

    fiche produit au clic du bouton acheter

--- frontend/produit.js
+++ frontend/produit.js
@@ -13,6 +13,8 @@ fetch('http://localhost:3000/api/' + categorie+"/"+id)
 })
 .then(function(data){
 
+//-----------------------------Visuel des element la page------------------------------------
+
 // Position des elements texte changé.
     document.querySelector("#leproduit h5").textContent = data.name;
     document.querySelector("#leproduit p").textContent = data.description;
@@ -20,9 +22,9 @@ fetch('http://localhost:3000/api/' + categorie+"/"+id)
     document.querySelector(".price").textContent = data.price / 100 + "€";
 //Déclaration d'une variable pour le tableau des optiond<;
     let options = data.lenses ;
+    console.log(options);
 // Boucle pour afficher les options
     for (let i = 0; i < options.length; i++) {
-        console.log(options);
         let option = options[i];
         let numoption = i+1;
         let positionopt = document.querySelector("#leproduit select");
@@ -31,4 +33,38 @@ fetch('http://localhost:3000/api/' + categorie+"/"+id)
         `
     }
     
+//----------------------Gestion du panier---------------------------------------------------------
+
+// Recupération des informations selectionnée a envoyer au panier.
+
+//selection de la liste des options du produit.
+const  idselection = document.querySelector(".form-select");
+const choixoption = idselection.value;
+console.log(choixoption);
+
+//recuparation Bouton Ajouter au panier  ! 
+
+const btn_envoyerpanier = document.querySelector("#leproduit button");
+console.log(btn_envoyerpanier);
+
+//Ecouter le bouton pour envoyer au panier 
+    btn_envoyerpanier.addEventListener("click", (event)=>{
+    event.preventDefault();
+    //selection de la liste des options du produit.
+    const  idselection = document.querySelector(".form-select");
+    const choixoption = idselection.value;
+    console.log(choixoption);
+    
+    //Récupération des valeurs lié au produit pour le panier.
+        let ficheproduit = {
+            nomproduit: data.name,
+            id_produit: id,
+            optionproduit: choixoption,
+            quantite: 1,
+            prix: data.price /100
+        }
+        console.log(ficheproduit);
+    })
+
+
 })
