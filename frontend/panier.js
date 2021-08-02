856d7809c23885e4a1ff2ed29fa496f7a678078d
Aris <arislebechec@gmail.com>
2021-08-02 16:52:21 +0200

    Ajout du bouton vider all panier

--- frontend/panier.js
+++ frontend/panier.js
@@ -40,7 +40,7 @@ if (produitenregistredanslocalstorage === null) {
         <td>${produitenregistredanslocalstorage[k].nomproduit}</td>
         <td>${produitenregistredanslocalstorage[k].optionproduit}</td>
         <td>${produitenregistredanslocalstorage[k].prix}</td>
-        <td>delete</td>
+        <td><button class="btn-supprimer">Supprimer</button></td>
     </tr>
         
         `;
@@ -51,12 +51,35 @@ if (produitenregistredanslocalstorage === null) {
     contenuHtml2.innerHTML = structureproduitpanier +`
     <tr>
                             <th scope="row">Prix total : </th>
-                            <th colspan="3">100</th>
-                            <td></td>
+                            <th colspan="2">100</th>
+                            <td colspan="2"><button class="btn-supprimer-all">Vider le panier</button></td>
                         </tr>
     `;
     
 }
 
+//-------------------------------------------GESTION DU BOUTON DELETE-------------------------------------------
+
+//Selection des réferences de tous  les boutons btn-supprimer
+ let btn_supprimer = document.querySelectorAll(".btn-supprimer");
+ 
+ //selection de L'ID a supprimer en fonction du bouton.
+
+/// ***********GESTION DU BOUTON DELETE ALL PANIER*************************
+
+//La selection de la référence du bouton "btn_supprimer_all".
+
+const btn_supprimer_all =  document.querySelector(".btn-supprimer-all")
+
+// Suppression  de la key "danspanier" dans le local storage.
+btn_supprimer_all.addEventListener("click", (e)=>{
+    e.preventDefault;
+
+    //.removeItem pour vider le local storage 
+    localStorage.removeItem("danspanier");
+    //alerte pour actualiser la page
+    alert("Le panier a bien été vidé");
+    window.location.href ="panier.html";
+})
 
 })
