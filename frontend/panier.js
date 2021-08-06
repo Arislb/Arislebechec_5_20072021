77da6e37a22abc334f692139802f715e86f87dc7
Aris <arislebechec@gmail.com>
2021-08-06 16:11:30 +0200

    Controle et validation du formulaire de commande

--- frontend/panier.js
+++ frontend/panier.js
@@ -96,9 +96,9 @@ for (let s = 0; s < produitenregistredanslocalstorage.length; s++) {
 }
 //Addtionner les pris qu'il y a dans le tableau"sommedupanier" avec la méthode .reducer
 const reducer = (accumulator, currentValue) => accumulator + currentValue;
-const Prixtotal = sommedupanier.reduce(reducer,0);
+const prixtotal = sommedupanier.reduce(reducer,0);
 
-document.querySelector(".prix-total").textContent = Prixtotal;
+document.querySelector(".prix-total").textContent = prixtotal;
 
 
 //-------------------------------Gestion formulaire---------------------------------------
@@ -125,7 +125,113 @@ btn_commande.addEventListener("click", (e)=>{
         city: city,
         email: document.querySelector("#inputmail").value,
         }
-        console.log(contact);
+        console.log("ici le contact"+contact);
+
+    const bcommande = {
+        produitenregistredanslocalstorage,
+        prixtotal,
+        contact,
+    }
+    console.log(bcommande);
+
+//-----------------------------------------------VALIDATION DU FORMULAIRE------------------------------------------------
+
+//FONCTION de controle et d'alert lié au formulaire
+
+//fonction regex qui controle le prenom nom et ville.
+const regexPNV = (value) =>{
+    return /^[A-Za-z]{3,20}$/.test(value);
+};
+// fonction regex qui controle lemail
+const regexmail = (value) =>{
+    return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(value);
+};
+//fonction regex qui controle l'adresse 
+const regexadress = (value) =>{
+return /^[A-Za-z0-9\s]{5,50}$/.test(value);
+};
+
+//fonction pour l'alert en cas d'erreur
+const pnvinval =(value)=>{
+    return value + `: Chiffre et symbole ne sont pas autorisé dans ce champ,3 caractères minimum`
+};
+
+// --- Controle des valeur mise dans le formulaire en true or false. 
+function control_name() {
+    const valfname = contact.firstName;
+    if(regexPNV(valfname)){
+    console.log(valfname);
+    return true;
+    }else{
+    
+    pnvinval("Nom")
+    console.log("ICIC FAUX prénom");
+    return false;
+    }
+    };
+
+function control_lastname() {
+    const vallname = contact.lastName;
+    console.log(vallname);
+    if(regexPNV(vallname)){
+        console.log(vallname);
+    return true;
+    }else{
+   
+    pnvinval("Prenom")
+    console.log("ICIC FAUX nom");
+    return false;
+    }
+    };
+
+function control_city(){
+    const valcity = contact.city;
+    if(regexPNV(valcity)){
+    console.log(valcity);
+    return true;
+    }else{
+    
+    pnvinval("Ville")
+    console.log("ICIC FAUXville");
+    return false;
+    }
+};
+
+function control_mail(){
+    const valmail = contact.email;
+    if(regexmail(valmail)){
+        console.log(valmail);
+        return true;
+        }else{
+        alert("L'email n'est pas valide")
+        console.log("ICIC FAUX mail");
+        return false;
+        }
+};
+
+function control_adress(){
+    const valadress = contact.address;
+    console.log(valadress);
+    if(regexadress(valadress)){
+        console.log(valadress);
+        return true;
+        }else{
+        alert("L'adresse n'est pas valide")
+        console.log("ICIC FAUX adress");
+        return false;
+        }
+};
+
+//---------------------------------------------FIN VALIDATION DU FORMULAIRE---------------
+
+if (control_name() && control_lastname() && control_city() && control_mail() && control_adress()) {
+    localStorage.setItem("bcommande", JSON.stringify(bcommande));
+}else{
+    alert("Veuillez bien remplir le formulaire")
+}
+//localStorage.setItem("bcommande", JSON.stringify(bcommande));
+
+//--------------------------------
 })
 
 
